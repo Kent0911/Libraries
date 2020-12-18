ce3311bfe52c733158de1d912598a22a441cd7d0
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2020-12-18 20:29:51 +0900

    initialize

---
+++ UserFormLib/Sources/InputCheck/inputCheck.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../Headers/defines.h"
+
+#include <string>
+#include <regex>
+
+bool checkMailAddress(std::string _str) {
+
+}
+
+bool checkAllAlphabet(std::string _str) {
+	std::smatch match;
+
+}
